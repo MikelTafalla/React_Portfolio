88c41d8567795fb3eddd71bddfdc22a2f50923a5
Mikel1991 <mikel362d@gmail.com>
2020-08-16 16:19:47 -0700

    add hero image to projects page

---
+++ src/pages/projects.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../components/Hero/index";
+
+function Projects() {
+    return (
+        <div>
+            <Hero backgroundImage="https://images.unsplash.com/photo-1461730278450-19cc1863601c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
+                <h1>Mikel Rodriguez</h1>
+                <h2>Welcome to my portfolio!</h2>
+            </Hero>
+
+        </div>
+    );
+}
+
+export default Projects;
