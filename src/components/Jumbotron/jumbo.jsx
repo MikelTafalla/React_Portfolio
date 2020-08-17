bc6f4b5d898030786dfdafd235e8641892e7dd10
Mikel1991 <mikel362d@gmail.com>
2020-08-17 12:34:58 -0700

    add links to about page

--- src/components/Jumbotron/jumbo.jsx
+++ src/components/Jumbotron/jumbo.jsx
@@ -7,8 +7,8 @@ const Jumbo = (props) => {
       <p className="lead">{props.message}</p>
       <hr className="my-4" />
       <p>{props.text}</p>
-      <a className="btn btn-success btn-lg" href="#" role="button">Learn more</a>
-      <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      <a className="btn btn-success btn-lg" href="/" role="button">See my projects</a>
+      <a className="btn btn-primary btn-lg" href="https://drive.google.com/file/d/1FXT9ZzMn6tvFQcJbAZbvFK_7vAgi1Cir/view" target="_blank" rel="noopener noreferrer" role="button">Online Resume</a>
     </div>
   )
 }
