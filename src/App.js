2eeb2be8b70fd35b329a4978b020e302e418f20f
Mikel1991 <mikel362d@gmail.com>
2020-08-18 12:15:49 -0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer/footer";
 import Project from "./pages/projects";
 import About from "./pages/about";
 import Contact from "./pages/contact";
@@ -14,8 +15,11 @@ function App() {
         <Route exact path="/projects" component={Project} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} /> 
+      <Footer />
     </div>
+   
   </Router>
+  
 );
 }
 
