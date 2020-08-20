7ebcefb521e983e8360547104fb4fdeba8fa5329
Mikel1991 <mikel362d@gmail.com>
2020-08-19 18:24:07 -0700

    Updates

---
+++ precache-manifest.069f038076265c5eebb06df0a46b8e16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd05b9b05398028fd5947c833fcb5537",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a0ff2579c16c501a93",
+    "url": "/static/css/main.727390ab.chunk.css"
+  },
+  {
+    "revision": "0cdf5353e955c098d417",
+    "url": "/static/js/2.2459e7fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2459e7fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a0ff2579c16c501a93",
+    "url": "/static/js/main.941b15ae.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9c37e8af7984eaabee4954464be43d9b",
+    "url": "/static/media/burgerApp.9c37e8af.gif"
+  },
+  {
+    "revision": "5c9cf600810361a69a59d4bb436cc45a",
+    "url": "/static/media/employeeDirectorygif.5c9cf600.gif"
+  },
+  {
+    "revision": "414193fb1e5c2670d84c48a8c113620a",
+    "url": "/static/media/employeeTrackergif.414193fb.gif"
+  },
+  {
+    "revision": "2985c6dabe0870eff7e0ba854e2dc64c",
+    "url": "/static/media/exerciseMegif.2985c6da.gif"
+  },
+  {
+    "revision": "88370f3467e509f4a0e9ec65308e281c",
+    "url": "/static/media/passwordgif.88370f34.gif"
+  },
+  {
+    "revision": "72d501d472801d77c74385c1661ad38a",
+    "url": "/static/media/weathergif.72d501d4.gif"
+  }
+]);
